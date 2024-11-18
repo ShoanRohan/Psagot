15d1d40893558a88d4f8e9abb27869a20c7516bc
BrachaAvitan <bracha53585@gmail.com>
2024-11-18 22:48:23 +0200

    Connect to AWS RDS Sql Server - Create Folder Models & added nugets.

---
+++ Entities/Contexts/PsagotDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Contexts;
+
+public partial class PsagotDbContext : DbContext
+{
+    public PsagotDbContext()
+    {
+    }
+
+    public PsagotDbContext(DbContextOptions<PsagotDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<UserType> UserTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=psagot.crc0ou6s0ptx.us-east-1.rds.amazonaws.com;Initial Catalog=psagot;User ID=admin;Password=admin2025;Trust Server Certificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Lecture>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Lectures__3214EC079AFB7810");
+
+            entity.Property(e => e.Name).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<UserType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__UserType__3214EC072EBE242C");
+
+            entity.ToTable("UserType");
+
+            entity.Property(e => e.Name).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
