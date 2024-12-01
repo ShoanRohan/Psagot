acc51fcd00d8afa4564d40b22ccfc953ae2f3e46
tamarbir <tammybirenbaum@gmail.com>
2024-12-01 13:32:07 +0200

    scaffold all tabels update scaffold in README

---
+++ Entities/Models/ScheduleForTopic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Models;
+
+public partial class ScheduleForTopic
+{
+    public int ScheduleForTopicId { get; set; }
+
+    public int TopicId { get; set; }
+
+    public int DayId { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    public virtual Day Day { get; set; } = null!;
+
+    public virtual ICollection<Meeting> Meetings { get; set; } = new List<Meeting>();
+
+    public virtual Topic Topic { get; set; } = null!;
+}
