53df2cdb4e4bc3313d058f6239d3e769998b7971
stCohen <stcohen92@gmail.com>
2024-12-02 22:15:28 +0200

    Resolved merge conflicts

--- Entities/MappingProfile.cs
+++ Entities/MappingProfile.cs
@@ -8,5 +8,6 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<UserType, UserTypeDTO>().ReverseMap();
+            CreateMap<CourseDTO, Course>().ReverseMap();
     }
 }
---
+++ Psagot/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Psagot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+
+
+        [HttpPost]
+    }
+}
