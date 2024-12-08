ee7f68bc4743fc79c7b44627f6dc5b9e6208ba0c
HadassaSabag <hadassa313@gmail.com>
2024-12-08 18:10:18 +0200

    Topic - DTO

---
+++ BL/TopicBL.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class TopicBL
+    {
+        private readonly ITopicDL _topicDL;
+        private readonly IMapper _mapper;
+
+        public TopicBL(ITopicDL topicDL, IMapper mapper)
+        {
+            _topicDL = topicDL;
+            _mapper = mapper;
+        }
+    }
+}
