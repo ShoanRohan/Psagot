43a83ca1af803d36d85198bb5344f765f4a639f8
Nava-cohen <navac5566@gmail.com>
2024-12-17 17:41:19 +0200

    first

---
+++ DL/RoomDL.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Contexts;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+
+
+    public class RoomDL : IRoomDL
+
+    {
+        private readonly PsagotDbContext _context;
+
+        public RoomDL (PsagotDbContext context)
+        {
+            _context = context;
+        }
+        public Task<(Room Room, string ErrorMessage)> AddRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(Room Room, string ErrorMessage)> UpdateRoom(Room room)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
