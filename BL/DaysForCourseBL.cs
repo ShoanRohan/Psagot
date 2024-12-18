4befaf502bc07beac1cc7b289f85092dde867ece
BrachaAvitan <bracha53585@gmail.com>
2024-12-18 23:32:46 +0200

    Revert to commit merge: Task 1: add function GetDaysForCourseByCourseId in DTO, BL, DL and Controller.

--- BL/DaysForCourseBL.cs
+++ BL/DaysForCourseBL.cs
@@ -1,7 +1,4 @@
-﻿using AutoMapper;
-using DL;
-using Entities.DTO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +8,5 @@ namespace BL
 {
     public class DaysForCourseBL : IDaysForCourseBL
     {
-        private readonly IDaysForCourseDL _daysforcourseDL;
-        private readonly IMapper _mapper;
-
-        public DaysForCourseBL(IDaysForCourseDL DaysForCourseDL, IMapper mapper)
-        {
-            _daysforcourseDL = DaysForCourseDL;
-            _mapper = mapper;
-        }
-        public async Task<(IEnumerable<DaysForCourseDTO> DaysForCourse, string ErrorMessage)> GetDaysForCourseByCourseId(int courseId)
-        {
-            var (DaysForCourse, errorMessage) = await _daysforcourseDL.GetDaysForCourseByCourseId(courseId);
-            if (DaysForCourse == null) return (null, errorMessage);
-
-            return (_mapper.Map<IEnumerable<DaysForCourseDTO>>(DaysForCourse), null);
-        }
     }
 }
