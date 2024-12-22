dda35017a39786877ee450107728b0ac1aa30eb9
tamarg627@gmail.com <tamarg627@gmail.com>
2024-12-22 21:14:38 +0200

    Add userAction

---
+++ psagot-client/src/utils/userUtil.js
@@ -0,0 +1,30 @@
+import api from "./api"
+
+const getAllUsers = async ()=> {
+    const response = await api.get('/User/GetAllUsers');
+    return response.data;
+};
+
+const getUserById = async (id) => {
+    const response = await api.get(`/User/GetUserById/${id}`);
+    return response.data;
+};
+
+const addUser = async (newUser) => {
+    const response = await api.post('/User/AddUser', newUser);
+    return response.data;
+};
+
+const updatedUser = async (updateUser) => {
+    const response = await api.put('/User/UpdateUser',updateUser);
+    return response.data;   
+};
+
+const deleteUser = async (id) =>{
+    const response = await api.delete(`/User/DeleteUser/${id}`)
+    return response.data;
+};
+
+export{getAllUsers, getUserById, addUser, updatedUser, deleteUser};
+
+
