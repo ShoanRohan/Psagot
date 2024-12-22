0c3ddea588ddb1c94ac41eea897f6cecaf411307
BrachaAvitan <bracha53585@gmail.com>
2024-12-23 00:34:48 +0200

    Task 2 - Add Templete DaysForCourse - Utils, Redux Slice & Actions.

--- psagot-client/src/features/store.js
+++ psagot-client/src/features/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userTypeReducer from '../features/userType/userTypeSlice';
+import daysForCourseReducer from '../features/daysForCourse/daysForCourseSlice';
 
 const store = configureStore({
   reducer: {
     userType: userTypeReducer, //Slice of userType
+    daysForCourse: daysForCourseReducer, //Slice of daysForCourse
   },
 });
 
