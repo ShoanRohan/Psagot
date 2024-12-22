3a7493ca57cc17ad723879f21717b23b0ae995a0
tmh15091 <tmh15091@gmail.com>
2024-12-22 21:39:06 +0200

    room reducer

---
+++ psagot-client/src/features/room/roomSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchAllRooms, fetchRoomById, addRoomAction, updateRoomAction } from './roomActions';
+
+const initialState = {
+    rooms: [],
+    selectedUser: null,
+    status: 'idle', // state connected: idle - מצב התחלתי, loading- בטעינה, succeeded - הצלחה, failed - נכשל
+    error: null,
+};
+
+const userSlice = createSlice({
+    name: 'room',
+    initialState,
+    reducers: {
+        
+        setUser: (state, action) => {
+            
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchAllRooms.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchAllRooms.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.rooms = action.payload;
+            })
+            .addCase(fetchAllRooms.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(fetchRoomById.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchRoomById.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.selectedUser = action.payload;
+            })
+            .addCase(fetchRoomById.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            })
+            .addCase(addRoomAction.fulfilled, (state, action) => {
+                state.rooms.push(action.payload);
+            })
+            .addCase(updateRoomAction.fulfilled, (state, action) => {
+                const index = state.rooms.findIndex((room) => room.id === action.payload.id);
+                if (index !== -1) {
+                    state.rooms[index] = action.payload;
+                }
+            });
+    },
+});
+
+export const { setRoom } = roomSlice.actions;
+export default roomSlice.reducer;
