7ab98772de19df4c26c527e42d68f896f9b2171c
BrachaAvitan <bracha53585@gmail.com>
2025-01-12 01:19:38 +0200

    fixed in Program.cs & removed space.

--- Psagot/Controllers/MeetingController.cs
+++ Psagot/Controllers/MeetingController.cs
@@ -44,7 +44,6 @@ namespace Psagot.Controllers
             return Ok(meeting);
         }
 
-
         [HttpGet("GetAllMeetings")]
         public async Task<IActionResult> GetAllMeetings()
         {
