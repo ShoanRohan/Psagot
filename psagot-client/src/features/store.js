31fa092f484eb86df386cd19c3486b99c8b3363e
extremeCase <belly57190@gmail.com>
2025-01-14 22:25:52 +0200

    nothing just try

--- psagot-client/src/features/store.js
+++ psagot-client/src/features/store.js
@@ -4,6 +4,8 @@ import userTypeReducer from '../features/userType/userTypeSlice';
 const store = configureStore({
   reducer: {
     userType: userTypeReducer, //Slice of userType
+    //nothing nothing nothing
+    aa
   },
 });
 
