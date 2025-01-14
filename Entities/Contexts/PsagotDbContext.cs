2b8e00bbc43e40e10346f57eeb26d64f4e788301
שושנה רצקר <shoshanar@moin.gov.il>
2025-01-14 16:08:02 +0200

    merge

--- Entities/Contexts/PsagotDbContext.cs
+++ Entities/Contexts/PsagotDbContext.cs
@@ -5,13 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Entities.Contexts;
 
-public partial class PsagotContext : DbContext
+public partial class PsagotDbContext : DbContext
 {
-    public PsagotContext()
-    {
-    }
-
-    public PsagotContext(DbContextOptions<PsagotContext> options)
+    public PsagotDbContext(DbContextOptions<PsagotDbContext> options)
         : base(options)
     {
     }
@@ -34,15 +30,11 @@ public partial class PsagotContext : DbContext
 
     public virtual DbSet<UserType> UserTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=PC-77114\\MSSQLSERVER01;Database=psagot;Trusted_Connection=True;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Course>(entity =>
         {
-            entity.HasKey(e => e.CourseId).HasName("PK__Courses__C92D71A742C778A6");
+            entity.HasKey(e => e.CourseId).HasName("PK__Courses__C92D71A7E4F1CA8D");
 
             entity.Property(e => e.Color).HasMaxLength(20);
             entity.Property(e => e.Name).HasMaxLength(50);
@@ -50,7 +42,7 @@ public partial class PsagotContext : DbContext
 
         modelBuilder.Entity<Day>(entity =>
         {
-            entity.HasKey(e => e.DayId).HasName("PK__Days__BF3DD8C540FA3E21");
+            entity.HasKey(e => e.DayId).HasName("PK__Days__BF3DD8C527726B75");
 
             entity.Property(e => e.Descr).HasMaxLength(10);
             entity.Property(e => e.Name).HasMaxLength(10);
@@ -58,85 +50,85 @@ public partial class PsagotContext : DbContext
 
         modelBuilder.Entity<DaysForCourse>(entity =>
         {
-            entity.HasKey(e => e.DaysForCourseId).HasName("PK__DaysForC__E8318208D335586E");
+            entity.HasKey(e => e.DaysForCourseId).HasName("PK__DaysForC__E83182085726CA61");
 
             entity.ToTable("DaysForCourse");
 
             entity.HasOne(d => d.Course).WithMany(p => p.DaysForCourses)
                 .HasForeignKey(d => d.CourseId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__DaysForCo__Cours__6383C8BA");
+                .HasConstraintName("FK__DaysForCo__Cours__4BAC3F29");
 
             entity.HasOne(d => d.Day).WithMany(p => p.DaysForCourses)
                 .HasForeignKey(d => d.DayId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__DaysForCo__DayId__628FA481");
+                .HasConstraintName("FK__DaysForCo__DayId__4AB81AF0");
         });
 
         modelBuilder.Entity<Meeting>(entity =>
         {
-            entity.HasKey(e => e.MeetingId).HasName("PK__Meetings__E9F9E94C2B845CDE");
+            entity.HasKey(e => e.MeetingId).HasName("PK__Meetings__E9F9E94C4C396225");
 
             entity.Property(e => e.IsValid).HasDefaultValue(true);
 
             entity.HasOne(d => d.Day).WithMany(p => p.Meetings)
                 .HasForeignKey(d => d.DayId)
-                .HasConstraintName("FK__Meetings__DayId__7A672E12");
+                .HasConstraintName("FK__Meetings__DayId__6477ECF3");
 
             entity.HasOne(d => d.Room).WithMany(p => p.Meetings)
                 .HasForeignKey(d => d.RoomId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Meetings__RoomId__797309D9");
+                .HasConstraintName("FK__Meetings__RoomId__6383C8BA");
 
             entity.HasOne(d => d.ScheduleForTopic).WithMany(p => p.Meetings)
                 .HasForeignKey(d => d.ScheduleForTopicId)
-                .HasConstraintName("FK__Meetings__Schedu__787EE5A0");
+                .HasConstraintName("FK__Meetings__Schedu__628FA481");
         });
 
         modelBuilder.Entity<Room>(entity =>
         {
-            entity.HasKey(e => e.RoomId).HasName("PK__Rooms__32863939B24E34D1");
+            entity.HasKey(e => e.RoomId).HasName("PK__Rooms__32863939245514A4");
 
             entity.Property(e => e.Name).HasMaxLength(50);
         });
 
         modelBuilder.Entity<ScheduleForTopic>(entity =>
         {
-            entity.HasKey(e => e.ScheduleForTopicId).HasName("PK__Schedule__3483C9F7A127A09A");
+            entity.HasKey(e => e.ScheduleForTopicId).HasName("PK__Schedule__3483C9F7B7C2104B");
 
             entity.ToTable("ScheduleForTopic");
 
             entity.HasOne(d => d.Day).WithMany(p => p.ScheduleForTopics)
                 .HasForeignKey(d => d.DayId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__ScheduleF__DayId__71D1E811");
+                .HasConstraintName("FK__ScheduleF__DayId__5BE2A6F2");
 
             entity.HasOne(d => d.Topic).WithMany(p => p.ScheduleForTopics)
                 .HasForeignKey(d => d.TopicId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__ScheduleF__Topic__70DDC3D8");
+                .HasConstraintName("FK__ScheduleF__Topic__5AEE82B9");
         });
 
         modelBuilder.Entity<Topic>(entity =>
         {
-            entity.HasKey(e => e.TopicId).HasName("PK__Topics__022E0F5D9CC5AF40");
+            entity.HasKey(e => e.TopicId).HasName("PK__Topics__022E0F5D0BC688FF");
 
             entity.Property(e => e.Name).HasMaxLength(50);
 
             entity.HasOne(d => d.Course).WithMany(p => p.Topics)
                 .HasForeignKey(d => d.CourseId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Topics__CourseId__6D0D32F4");
+                .HasConstraintName("FK__Topics__CourseId__571DF1D5");
 
             entity.HasOne(d => d.Teacher).WithMany(p => p.Topics)
                 .HasForeignKey(d => d.TeacherId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Topics__TeacherI__6E01572D");
+                .HasConstraintName("FK__Topics__TeacherI__5812160E");
         });
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4C2D1A147F");
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4CA9A03F49");
 
             entity.Property(e => e.Email).HasMaxLength(100);
             entity.Property(e => e.IsActive).HasDefaultValue(true);
@@ -147,12 +139,12 @@ public partial class PsagotContext : DbContext
             entity.HasOne(d => d.UserType).WithMany(p => p.Users)
                 .HasForeignKey(d => d.UserTypeId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Users__UserTypeI__6754599E");
+                .HasConstraintName("FK__Users__UserTypeI__5165187F");
         });
 
         modelBuilder.Entity<UserType>(entity =>
         {
-            entity.HasKey(e => e.UserTypeId).HasName("PK__UserType__40D2D816CED5660F");
+            entity.HasKey(e => e.UserTypeId).HasName("PK__UserType__40D2D816F047B536");
 
             entity.Property(e => e.Name).HasMaxLength(50);
         });
