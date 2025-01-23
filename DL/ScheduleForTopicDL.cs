441020b40c92f3985380d24da6ea2265dfa04007
miri_f <miri2033@gmail.com>
2025-01-23 10:44:56 +0200

    adding DELETE method for ScheduleForTopic

--- DL/ScheduleForTopicDL.cs
+++ DL/ScheduleForTopicDL.cs
@@ -18,6 +18,27 @@ namespace DL
             _context = context;
         }
 
+        public async Task<(bool IsDeleted, string ErrorMessage)> DeleteScheduleForTopic(int TopicId)
+        {
+            try
+            {
+                var scheduleForTopic = await _context.Set<ScheduleForTopic>().FindAsync(TopicId);
+                if (scheduleForTopic == null)
+                {
+                    return (false, "Schedule for topic not found.");
+                }
+
+                _context.Set<ScheduleForTopic>().Remove(scheduleForTopic);
+                await _context.SaveChangesAsync();
+
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
         public async Task<(ScheduleForTopic ScheduleForTopic, string ErrorMessage)> GetScheduleForTopicById(int id)
         {
             try
