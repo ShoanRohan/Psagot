a574e64108d1b48c9c40efa405f852d38bbb1f7e
Ahuva-Swiatycki <A0556772926@gmail.com>
2025-01-26 21:28:28 +0200

    login Changes

--- Psagot/Controllers/UserController.cs
+++ Psagot/Controllers/UserController.cs
@@ -2,6 +2,10 @@
 using Entities.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Psagot.Controllers
 {
@@ -19,10 +23,24 @@ namespace Psagot.Controllers
         [HttpPost("AddUser")]
         public async Task<IActionResult> AddUser([FromBody] UserDTO userDTO)
         {
-            var (addedUser, errorMessage) = await _userBL.AddUser(userDTO);
-            if (addedUser == null) return BadRequest(errorMessage);
+            if (userDTO == null)
+                return BadRequest("Invalid user data");
+            try
+            {
+                var (addedUser, errorMessage) = await _userBL.AddUser(userDTO);
+                if (addedUser == null)
+                    return BadRequest(errorMessage);
 
-            return Ok(addedUser);
+                return CreatedAtAction(nameof(AddUser), new { id = addedUser.UserId }, new
+                {
+                    User = addedUser
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Internal server error");
+            }
         }
 
         [HttpPut("UpdateUser")]
@@ -52,6 +70,28 @@ namespace Psagot.Controllers
             return Ok(users);
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDTO login)
+        {
+            if (login == null)
+                return BadRequest("Invalid login request");
 
+            try
+            {
+                var user = await _userBL.UserLoginAsync(login.Email, login.Password);
+
+                if (user == null)
+                    return Unauthorized("Invalid email or password");
+                return Ok(new
+                {
+                    user = user,
+                });
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Internal server error");
+            }
+        }
     }
 }
