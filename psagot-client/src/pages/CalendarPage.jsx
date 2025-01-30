066533e1402037df31057f7d8a5d0d5678e3d7d4
EstiBerenshtock <besti7703@gmail.com>
2025-01-30 22:46:48 +0200

    create new component and page of calendar

---
+++ psagot-client/src/pages/CalendarPage.jsx
@@ -0,0 +1,11 @@
+import Calendar from "../components/Calendar"
+
+const CalendarPage=()=>{
+    return (
+        <div>
+            <h1>לוח שנה</h1>
+            <Calendar/>
+        </div>
+    )
+};
+export default CalendarPage;
