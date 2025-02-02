344f3ecb1d4f4b304b9da0c394847efdffffb980
stCohen <stcohen92@gmail.com>
2025-02-02 21:08:30 +0200

    upDate

--- psagot-client/src/utils/courseUtil.js
+++ psagot-client/src/utils/courseUtil.js
@@ -4,15 +4,10 @@ const getCourseById = async (id) => {
     const response = await api.get(`/Course/GetCourseById/${id}`);
     return response.data;
 };
-export { getCourseById}
-
-import api from "./api";
-   
-
 
 const updateCourse= async (updateCourse) => {
     const response = await api.put(`/Course/UpdateCourse`, updateCourse);
     return response.data;
 };
 
-export{updateCourse};
+export{updateCourse,getCourseById};
