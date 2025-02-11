4fff8e8a2b068248d74233d893211359651509eb
chagit4213 <limudimchagit4213@gmail.com>
2025-02-11 22:17:37 +0200

    בדיקה

--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
+import RoomsGrid from '../components/RoomsGrid';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-                <Route path='/' element={<HomePage/>}/>
+                <Route path='/' element={<RoomsGrid/>}/>
             </Route>
         </Routes>
     );
