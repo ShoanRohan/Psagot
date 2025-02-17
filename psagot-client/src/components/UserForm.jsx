c7f8de59e7b3793b37df379ace7ddfb73c58235a
EsterSimon <esty7858@gmail.com>
2025-02-17 17:23:46 +0200

    creat user form

---
+++ psagot-client/src/components/UserForm.jsx
@@ -0,0 +1,22 @@
+import { Email } from "@mui/icons-material";
+import React from "react";
+
+const haldelSumbit = (event)=>{
+    event.preventDefault();
+
+    const userData = {
+        Name: formData.name,
+        Email: formData.Email
+        
+        
+    };
+}
+const UserForm = () => {
+    return(
+        <>
+        </>
+    )
+
+};
+
+
