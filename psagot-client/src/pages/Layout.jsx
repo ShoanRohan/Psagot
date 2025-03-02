ab434919f8b05dd0074d9688410635f7df000d21
tamarbir <tammybirenbaum@gmail.com>
2025-03-02 10:21:17 +0200

    Update Layout.jsx

--- psagot-client/src/pages/Layout.jsx
+++ psagot-client/src/pages/Layout.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Section from "../components/Section";
-import Login  from "../components/Login";
 import Footer from "../components/Footer";
 import { Box } from "@mui/material";
-import LoginPage from "../pages/LoginPage";
 
 const Layout = () => {
     return (
