e25e284e952f09026144824790834a60f2a224e3
bracha leizerowitz <brachil3079@gmail.com>
2025-03-05 10:12:07 +0200

    A few more fixes & design to the folder

--- psagot-client/src/pages/EditUser.jsx
+++ psagot-client/src/pages/EditUser.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { getUserById, updatedUser } from "../utils/userUtil";
+import "../styles/editUser.css";
 
 const EditUser = () => {
     const { id } = useParams(); // קבלת מזהה המשתמש מה-URL
@@ -9,6 +10,7 @@ const EditUser = () => {
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const location = useLocation();
     
     useEffect(() => {
         fetchUser();
@@ -35,6 +37,13 @@ const EditUser = () => {
             setError("שגיאה בעדכון המשתמש");
         }
     };
+    const handleCancel = () => {
+        if (location.state?.from === 'users') {
+            navigate('/users');
+        } else {
+            navigate('/'); // דף הבית
+        }
+    };
 
     if (loading) return <p>טוען נתונים...</p>;
     if (error) return <p>{error}</p>;
@@ -44,31 +53,31 @@ const EditUser = () => {
             <h2>עריכת משתמש</h2>
 
             <label>קוד משתמש:</label>
-            <input type="id" value={user?.userId} onChange={(e) => setUser({ ...user, userId: e.target.value })} />
+            <input type="id" value={user?.userId} onChange={(e) => setUser({ ...user, userId: e.target.value })}className="input-place"   />
 
             <label>שם משתמש:</label>
-            <input type="name" value={user?.name} onChange={(e) => setUser({ ...user, name: e.target.value })} />
+            <input type="name" value={user?.name} onChange={(e) => setUser({ ...user, name: e.target.value })}   placeholder="שם"  className="input-place"   />
 
             <label>אימייל:</label>
-            <input type="email" value={user?.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
+            <input type="email" value={user?.email} onChange={(e) => setUser({ ...user, email: e.target.value })} className="input-place"  />
 
             <label>טלפון:</label>
-            <input type="phone" value={user?.phone} onChange={(e) => setUser({ ...user, phone: e.target.value })} />
+            <input type="phone" value={user?.phone} onChange={(e) => setUser({ ...user, phone: e.target.value })} className="input-place"   />
 
             <label>סיסמא:</label>
-            <input type="password" value={user?.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
+            <input type="password" value={user?.password} onChange={(e) => setUser({ ...user, password: e.target.value })} className="input-place"  />
 
             <label> קוד סוג משתמש:</label>
-            <input type="usertypeId" value={user?.userTypeId} onChange={(e) => setUser({ ...user, userTypeId: e.target.value })} />
+            <input type="usertypeId" value={user?.userTypeId} onChange={(e) => setUser({ ...user, userTypeId: e.target.value })}className="input-place"   />
 
             <label>:סוג משתמש</label>
-            <input type="userType" value={user?.userTypeName} onChange={(e) => setUser({ ...user, userTypeName: e.target.value })} />
+            <input type="userType" value={user?.userTypeName} onChange={(e) => setUser({ ...user, userTypeName: e.target.value })}className="input-place"   />
 
             <label>פעיל:</label>
-            <input type="isActive?" value={user?.isActive} onChange={(e) => setUser({ ...user, isActive: e.target.value })} />
+            <input type="isActive?" value={user?.isActive} onChange={(e) => setUser({ ...user, isActive: e.target.value })}className="input-place"   />
 
             <label>תפקיד:</label>
-            <input type="role" value={user?.role} onChange={(e) => setUser({ ...user, role: e.target.value })} />
+            <input type="role" value={user?.role} onChange={(e) => setUser({ ...user, role: e.target.value })}className="input-place"   />
 
             {/* <label>הרשאה:</label>
             <select value={user.role} onChange={(e) => setUser({ ...user, role: e.target.value })}>
@@ -77,7 +86,7 @@ const EditUser = () => {
             </select> */}
 
             <button onClick={handleSave}>שמירה</button>
-            <button onClick={() => navigate("/users")}>ביטול</button>
+            <button onClick={handleCancel}>ביטול</button>
         </div>
     );
 };
