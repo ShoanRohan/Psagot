c3068bc484e4ff9683a36a05b253232f409500f6
ofratAct <ofrathava@gmail.com>
2025-03-17 21:07:44 +0200

    7.1 7.3 7.3.1

---
+++ psagot-client/src/components/CourseScreen.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Button, Typography, Tabs, Tab, IconButton } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import CourseDetails from "./CourseDetails";
+import CourseTopics from "./CourseTopics";
+import "./CourseScreen.css"; // ייבוא עיצוב
+
+const CourseScreen = () => {
+  const [tabIndex, setTabIndex] = React.useState(0);
+
+  const handleTabChange = (event, newIndex) => {
+    setTabIndex(newIndex);
+  };
+
+  return (
+    <Box className="course-container">
+      {/* כותרת הקורס */}
+      <Box className="course-header">
+        <Box>
+          <Typography className="course-title">קורס עיצוב</Typography>
+          <Typography className="course-status">סטטוס הקורס: פעיל</Typography>
+        </Box>
+        <Box className="course-actions">
+          <Button variant="contained" startIcon={<AddCircleOutlineIcon />} className="add-topic-btn">
+            הוספת נושא
+          </Button>
+          <IconButton>
+            <LockOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+
+      {/* בר ניווט */}
+      <Tabs value={tabIndex} onChange={handleTabChange} className="course-tabs">
+        <Tab label="פרטי קורס" className="course-tab" />
+        <Tab label="נושאים וחומרים" className="course-tab" />
+      </Tabs>
+
+      {/* תוכן בהתאם ללשונית */}
+      <Box mt={2}>
+        {tabIndex === 0 && <CourseDetails />}
+        {tabIndex === 1 && <CourseTopics />}
+      </Box>
+    </Box>
+  );
+};
+
+export default CourseScreen;
--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -3,12 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
 import TopicsSearch from '../components/TopicsSearch'
+import CourseScreen from '../components/CourseScreen';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-                <Route path='/' element={<TopicsSearch id={1}/>}/>
+            <Route path='/' element={<CourseScreen />}/>
+            <Route path='/TopicsSearch' element={<TopicsSearch id={1}/>}/>
+                
             </Route>
         </Routes>
     );
