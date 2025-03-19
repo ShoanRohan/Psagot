d93fe8e628e7394d7449a4fae4b2cecc2556f739
MichalShushan12 <michal200315@gmail.com>
2025-03-19 16:47:10 +0200

    fix issues

--- BL/CourseBL.cs
+++ BL/CourseBL.cs
@@ -57,15 +57,7 @@ namespace BL
         }
 
 
-        public async Task<(CourseDTO Course, string ErrorMessage)> UpdateCourse(CourseDTO course)
-        {
-            var courseDetails = _mapper.Map<Course>(course);
-            var (updatedCourse, errorMessage) = await _courseDL.UpdateCourse(courseDetails);
 
-            if (updatedCourse == null) return (null, errorMessage);
-
-            return (_mapper.Map<CourseDTO>(updatedCourse), null);
-        }
 
     }
 }
