8af334d311222b8a977d6ba4c60655b8dcb65bb9
rvblum <rivky2898@gmail.com>
2025-03-23 21:30:51 +0200

    הוספת איקון

--- psagot-client/src/components/CoursePage.jsx
+++ psagot-client/src/components/CoursePage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import CourseSearch from "./CourseSearch";
-import CoursesTable from "./CourseTable";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import GridOnIcon from "@mui/icons-material/GridOn"; // אייקון אקסל
 import { Stack } from "@mui/material"; // מסדר כפתורים בשורה
-
+import CourseGrid from "./CourseGrid";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 const buttonStyles = {
   height: "44px",
   padding: "0px 20px",
@@ -29,51 +29,49 @@ const buttonStyles = {
     boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.3)",
   },
 };
-
 const CoursesPage = () => {
   return (
     <Box sx={{ p: 3 }}>
       {/* כותרת עם כפתורים */}
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "16px",
-          width: "105%",
-          marginX: "auto",
-        }}
-      >
-        <Typography
-          variant="h1"
-          align="right"
-          sx={{
-            fontFamily: "Rubik, sans-serif",
-            fontWeight: 700,
-            fontSize: "40px",
-            color: "#0D1783",
-          }}
-        >
+             sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: "16px",
+              width: "105%",
+              marginX: "auto",
+            }}
+          >
+            <Typography
+              variant="h1"
+              align="right"
+              sx={{
+                fontFamily: "Rubik, sans-serif",
+                fontWeight: 700,
+                fontSize: "40px",
+                color: "#0D1783",
+              }}
+            >
           קורסים
         </Typography>
-
         {/* כפתורים בצד שמאל */}
         <Stack direction="row" spacing={2}>
           {/* כפתור עגול עם אייקון בלבד */}
           <IconButton >
             <GridOnIcon />
           </IconButton>
-
-          <Button variant="contained" sx={buttonStyles}>
+          <Button variant="contained" sx={buttonStyles}
+                    startIcon={<AddCircleOutlineIcon />}
+>
             הוספת קורס
+
           </Button>
         </Stack>
       </Box>
-
       <CourseSearch />
-      <CourseGrid />
-  </div>
-    );
+      <CourseGrid/>
+    </Box>
+  );
 };
-
 export default CoursesPage;
