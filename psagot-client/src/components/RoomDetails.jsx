73e2c2d7238cb77ee592cb3b92a56e3b0308bb14
RachelTaieb <rachelna102@gmail.com>
2025-04-23 20:43:07 +0300

    RoomDatails Component

--- psagot-client/src/components/RoomDetails.jsx
+++ psagot-client/src/components/RoomDetails.jsx
@@ -1,76 +1,44 @@
-import React, { useState } from 'react';
-import './RoomDetails.css'; // קובץ CSS להוספת עיצוב
+
+
+import React, { useState } from "react";
+import { TextField, MenuItem, Typography, Box } from "@mui/material";
+import "./RoomDetails.css"; 
 
 const RoomDetails = () => {
-    const [roomName, setRoomName] = useState('');
-    const [roomNumber, setRoomNumber] = useState('');
-    const [placesNumber, setPlacesNumber] = useState('');
-    const [selectedEquipment, setSelectedEquipment] = useState([]);
+  const [equipment, setEquipment] = useState([]);
 
-    const equipmentList = [
-        { id: 1, name: 'מקרן' },
-        { id: 2, name: 'מזגן' },
-        { id: 3, name: 'מחשב' },
-        { id: 4, name: 'לוח' },
-    ];
+  const handleEquipmentChange = (event) => {
+    setEquipment(event.target.value);
+  };
 
-    const handleCheckboxChange = (event, equipmentId) => {
-        if (event.target.checked) {
-            setSelectedEquipment([...selectedEquipment, equipmentId]);
-        } else {
-            setSelectedEquipment(selectedEquipment.filter(id => id !== equipmentId));
-        }
-    };
+  return (
+    <Box className="page-container">
+      <Box className="room-details-container">
+        <Typography className="room-details-title">פרטים טכניים</Typography>
 
-    return (
-        <div className="room-details-container">
-            {/* סרגל */}
-            <div className="room-bar"></div>
-
-            {/* שדות הקלדה */}
-            <div className="input-group">
-                <label>שם חדר:</label>
-                <input
-                    type="text"
-                    value={roomName}
-                    onChange={(e) => setRoomName(e.target.value)}
-                />
-            </div>
-
-            <div id="RoomNumber" className="input-group">
-                <label>מספר חדר:</label>
-                <input
-                    type="number"
-                    value={roomNumber}
-                    onChange={(e) => setRoomNumber(e.target.value)}
-                />
-            </div>
-
-            <div className="input-group">
-                <label>מספר מקומות:</label>
-                <input
-                    type="number"
-                    value={placesNumber}
-                    onChange={(e) => setPlacesNumber(e.target.value)}
-                />
-            </div>
-
-            {/* סלקט עם צ'קבוקסים */}
-            <div className="equipment-group">
-                <label>אביזרי ציוד:</label>
-                {equipmentList.map((equipment) => (
-                    <div key={equipment.id} className="equipment-item">
-                        <input
-                            type="checkbox"
-                            id={equipment - equipment.id}
-                          onChange={(e) => handleCheckboxChange(e, equipment.id)}
-                        />
-                        <label htmlFor={`equipment-${equipment.id}`}>{equipment.name}</label>
-                    </div>
-                ))}
-            
-            </div>
-            </div>)
+        <Box className="room-details-form">
+          <TextField label="שם חדר" variant="standard" className="text-field" InputProps={{ className: "text-input" }} />
+          <TextField label="מספר חדר" variant="standard" type="number" className="text-field" InputProps={{ className: "text-input" }} />
+          <TextField label="מספר מקומות" variant="standard" type="number" className="text-field" InputProps={{ className: "text-input" }} />
+          
+          <TextField
+            select
+            label="ציוד"
+            variant="standard"
+            className="text-field"
+            SelectProps={{ multiple: true }}
+            value={equipment}
+            onChange={handleEquipmentChange}
+            InputProps={{ className: "text-input" }}
+          >
+            <MenuItem value="מקרן">מקרן</MenuItem>
+            <MenuItem value="לוח">לוח</MenuItem>
+            <MenuItem value="מחשב">מחשב</MenuItem>
+          </TextField>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
-            export default RoomDetails;
+export default RoomDetails;
