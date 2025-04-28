407765ba6f76dfcf0392a331bb1b48db68c2c576
MichalShushan12 <michal200315@gmail.com>
2025-04-28 15:53:11 +0300

    Sve local changing before merging teamJ2

--- psagot-client/src/pages/Layout.jsx
+++ psagot-client/src/pages/Layout.jsx
@@ -3,15 +3,15 @@ import Header from "../components/Header";
 import Section from "../components/Section";
 import Footer from "../components/Footer";
 import { Box } from "@mui/material";
-import AddMeeting from "./AddMeeting";
 import MeetingButton from "./MeetingButton";
-import MeetingLocatorBar from "./MeetingLocatorBar";
+
 
 const Layout = () => {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Header />
             <Section />
+
             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             <MeetingButton />
             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
@@ -22,7 +22,3 @@ const Layout = () => {
 
 export default Layout;
 
-/*
-  <Section />
-            <Footer />
-*/
--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
-import AddMeeting from '../pages/AddMeeting';
 import MeetingButton from '../pages/MeetingButton';
-import MeetingLocatorBar from '../pages/MeetingLocatorBar';
+
 
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/AddMeeting' element={<AddMeeting/>}/>
+           
+            <Route path='/' element={<HomePage/>}/>           
             </Route>
         </Routes>
     );
 };
 
 export default AppRouter;
+
+// <Route path='/' element={<HomePage/>}/>
