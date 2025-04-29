071a9eacb9f64fd37b917906d8e45db8c0cceffe
tehila-sh <tehila2874@gmail.com>
2025-04-29 21:55:41 +0300

    PAS ITUR

--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -3,12 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
 import RoomsScheduleSearch from '../components/RoomsScheduleSearch';
+import RoomsPage from '../pages/RoomsPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-                <Route path='/' element={<RoomsScheduleSearch/>}/>
+                <Route path='/' element={<RoomsPage/>}/>
             </Route>
         </Routes>
     );
