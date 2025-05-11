71606d3dc916432624725dc13099af8621d85186
tamarg627@gmail.com <tamarg627@gmail.com>
2025-05-11 20:54:19 +0300

    add create new course

---
+++ psagot-client/src/components/CreateCourse.jsx
@@ -0,0 +1,284 @@
+import { Checkbox, Paper, Stack, TextField, Typography, MenuItem, Button, Box } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addCourseAction } from "../features/course/courseActions";
+
+const CreateCourse = () => {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    coordinatorName: "",
+    courseName: "",
+    courseCode: "",
+    startDate: "",
+    endDate: "",
+    year: "",
+    meetingsCount: "",
+    studentsCount: "",
+    notes: "",
+    status: "",
+    color: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    dispatch(addCourseAction(formData));
+  };
+
+  const statuses = ["פעיל", "לא פעיל", "ממתין"];
+
+  return (
+    <Paper
+       elevation={0}
+       dir="rtl"
+       sx={{
+        display: 'inline-flex',
+        padding: '40px',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        gap: '1.25rem',
+        borderRadius: '10px',
+        border: '1px solid #C6C6C6',
+        background: '#FFF',
+        boxShadow: '0px 0px 4px 0px rgba(220, 226, 236, 0.80)',
+        width: { xs: 'auto', sm: 'auto', md: 'auto', lg: '70vw'},
+        height: { xs: 'auto', sm: 'auto', md: 'auto', lg: '68vh'},
+      }}
+    >
+      <Typography variant='body1' fontWeight='bold'
+      sx={{
+        textAlign: "right",
+        fontFamily: 'Rubik',
+         fontSize: {xs: '1.5rem', sm: '1.75rem', md: '2rem'},
+        fontStyle: 'normal',
+        fontWeight:700,
+        lineHeight: 'normal',
+        textTransform: 'capitalize',
+        width: '100%',
+        color: '#0D1783',
+         mt: {xs: 1, sm: 2, md: 1},
+         mb: {xs: 1, sm: 2, md: 1},
+      }}>
+        הוספת קורס
+      </Typography>
+      
+      {/* Top Row */}
+      <Stack
+      direction="row"
+      justifyContent="flex-end"
+      alignItems="center"
+      >
+        <TextField
+        name="courseCode"
+        label="קוד קורס"
+        variant="standard"
+        value={formData.courseCode}
+        onChange={handleChange}
+        inputProps={{ dir: "rtl" }}
+        InputLabelProps={{ sx: { right: 0 } }}
+        sx={{ width: '200px' }}
+        />
+ 
+  <Box sx={{ mr: 2 }}>
+    <TextField
+      name="courseName"
+      label="שם קורס"
+      variant="standard"
+      value={formData.courseName}
+      onChange={handleChange}
+      inputProps={{ dir: "rtl" }}
+      InputLabelProps={{ sx: { right: 0 } }}
+      sx={{ width: '200px' }}
+    />
+  </Box>
+  <Box sx={{ mr: 4 }}>
+  <TextField
+    name="coordinatorName"
+    label="שם רכזת"
+    variant="standard"
+    value={formData.coordinatorName}
+    onChange={handleChange}
+    inputProps={{ dir: "rtl" }}
+    InputLabelProps={{ sx: { right: 0 } }}
+    sx={{ width: '200px' }}
+  />
+  </Box>
+</Stack>
+
+
+
+      {/* Middle Row */}
+<Stack direction="row" justifyContent="flex-end" alignItems="center">
+  <TextField
+    name="year"
+    label="שנה"
+    variant="standard"
+    value={formData.year}
+    onChange={handleChange}
+    inputProps={{ dir: "rtl" }}
+    InputLabelProps={{ sx: { right: 0 } }}
+    sx={{ width: '200px' }}
+  />
+  <Box sx={{ mr: 2 }}>
+    <TextField
+      name="startDate"
+      label="תאריך התחלה"
+      type="date"
+      variant="standard"
+      value={formData.startDate}
+      onChange={handleChange}
+      InputLabelProps={{ shrink: true, sx: { right: 0 } }}
+      sx={{ width: '200px' }}
+      inputProps={{ dir: "rtl" }}
+    />
+  </Box>
+  <Box sx={{ mr: 4 }}>
+    <TextField
+      name="endDate"
+      label="תאריך סיום"
+      type="date"
+      variant="standard"
+      value={formData.endDate}
+      onChange={handleChange}
+      InputLabelProps={{ shrink: true, sx: { right: 0 } }}
+      sx={{ width: '200px' }}
+      inputProps={{ dir: "rtl" }}
+    />
+  </Box>
+</Stack>
+
+
+     {/* Third Row */}
+<Stack direction="row" justifyContent="flex-end" alignItems="center">
+  <TextField
+    name="studentsCount"
+    label="מספר תלמידים"
+    type="number"
+    variant="standard"
+    value={formData.studentsCount}
+    onChange={handleChange}
+    inputProps={{ dir: "rtl" }}
+    InputLabelProps={{ sx: { right: 0 } }}
+    sx={{ width: '200px' }}
+  />
+  <Box sx={{ mr: 2 }}>
+    <TextField
+      name="meetingsCount"
+      label="מספר מפגשים"
+      type="number"
+      variant="standard"
+      value={formData.meetingsCount}
+      onChange={handleChange}
+      inputProps={{ dir: "rtl" }}
+      InputLabelProps={{ sx: { right: 0 } }}
+      sx={{ width: '200px' }}
+    />
+  </Box>
+</Stack>
+
+      {/* Notes */}
+      <TextField
+        name="notes"
+        label="הערות"
+        variant="standard"
+        multiline
+        rows={3}
+        value={formData.notes}
+        onChange={handleChange}
+        sx={{ width: 414 }}
+        inputProps={{ dir: "rtl" }}
+        InputLabelProps={{ sx: { right: 0 } }}
+      />
+
+      {/* Status and Checkbox */}
+      <Stack direction="row" spacing={3} alignItems="center">
+        <TextField
+          select
+          name="status"
+          label="סטטוס"
+          variant="standard"
+          value={formData.status}
+          onChange={handleChange}
+          sx={{ width: 200,
+            '& .MuiSelect-icon': {
+      left: 0,
+      right: 'auto',
+    },
+          }}
+          InputLabelProps={{ sx: { right: 0 } }}
+          
+        >
+          {statuses.map((s) => (
+            <MenuItem key={s} value={s}>
+              {s}
+            </MenuItem>
+          ))},
+          
+          
+        </TextField>
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Checkbox
+            checked={formData.color}
+            name="color"
+            onChange={handleChange}
+          />
+          <Typography variant="body2" sx={{ textAlign: "right" }}>
+            צבע לטבלה
+          </Typography>
+        </Stack>
+      </Stack>
+      <Stack
+  direction="row"
+  spacing={2}
+  justifyContent="center"
+  sx={{
+    width: '100%',
+   // mt: 4,
+  }}
+>
+  <Button
+    variant="outlined"
+    onClick={() => {/* למשל: navigate(-1) */}}
+    sx={{
+      height: '44px',
+      padding: '0px 24px',
+      borderRadius: '50px',
+      border: '1px solid #326DEF',
+      color: '#326DEF',
+      fontWeight: 500,
+    }}
+  >
+    ביטול
+  </Button>
+
+  <Button
+    variant="contained"
+    onClick={handleSubmit}
+    sx={{
+      height: '44px',
+      padding: '0px 24px',
+      borderRadius: '50px',
+      background: '#326DEF',
+      color: '#fff',
+      fontWeight: 500,
+      '&:hover': {
+        background: '#285ac0',
+      },
+    }}
+  >
+    שמור
+  </Button>
+</Stack>
+    </Paper>
+  );
+};
+
+export default CreateCourse;
--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import Layout from '../pages/Layout';
 import Login from '../components/Login';
 import LoginPage from '../pages/LoginPage';
 import UserManagement from '../pages/UserManagement';
+import CreateCourse from '../components/CreateCourse';
 
 const AppRouter = () => {
     return (
@@ -13,6 +14,7 @@ const AppRouter = () => {
              <Route path='/' element={<Layout/>}>
                 <Route path='/home' element={<HomePage />} />  
                 <Route path='user' element={<UserManagement />}/>
+                <Route path='newCourse' element={<CreateCourse />}/>
             </Route> 
                 
             <Route path='/' element={<LoginPage/>}> 
