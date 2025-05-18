b9e75f1f38aa6ec7efd903022a83566494798a5d
שושנה רצקר <shoshanar@moin.gov.il>
2025-05-18 13:32:22 +0300

    route

--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -4,13 +4,14 @@ import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
 import CalendarPage from '../pages/CalendarPage';
 import CourseScreen from "../components/CourseScreen";
+import CoursesPage from '../components/CoursesPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout />}>
                 <Route path='/' element={<HomePage />} />
-                <Route path='/courses' element={<span>page Courses</span>} />
+                <Route path='/courses' element={<CoursesPage />} />
                 <Route path='/meetings' element={<span>page Meetings</span>} />
                 <Route path='/users' element={<span>page Users</span>} />
                 <Route path='/rooms' element={<span>page Rooms</span>} />
