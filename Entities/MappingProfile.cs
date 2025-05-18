c492c48e3f47790beff06c7983fca9702d0a81bf
Ahuva-Swiatycki <A0556772926@gmail.com>
2025-05-18 22:56:37 +0300

    no changes

--- Entities/MappingProfile.cs
+++ Entities/MappingProfile.cs
@@ -8,8 +8,10 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<UserType, UserTypeDTO>().ReverseMap();
-        CreateMap<User, UserDTO>().ForMember(dest => dest.UserTypeName,
-                opt => opt.MapFrom(src => src.UserType.Name)).ReverseMap();
+        CreateMap<User, UserDTO>()
+     .ForMember(dest => dest.UserTypeName,
+         opt => opt.MapFrom(src => src.UserType != null ? src.UserType.Name : null))
+     .ReverseMap();
         CreateMap<Room, RoomDTO>().ReverseMap();
         CreateMap<Day, DayDTO>().ReverseMap();
         CreateMap<Meeting, MeetingDTO>().ReverseMap();
