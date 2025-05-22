fb730fbfa3fe9139326c0adb5382a7120e9f8570
YafitShama <yafit.h2112@gmail.com>
2025-05-22 18:58:00 +0300

    get all active courses

--- psagot-client/src/App.js
+++ psagot-client/src/App.js
@@ -4,13 +4,16 @@ import AppRouter from './routers/AppRouter';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import CourseSearch from './components/CourseSearch';
+import CourseList from './components/CourseList';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         {/* <AppRouter /> */}
+        <CourseList>
         <CourseSearch></CourseSearch>
+        </CourseList>
       </BrowserRouter>
     </Provider>
   );
