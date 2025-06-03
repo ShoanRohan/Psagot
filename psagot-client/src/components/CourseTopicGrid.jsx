4a4f913674af35ffa5d8711ff1b54b6c7aad1d38
שושנה רצקר <shoshanar@moin.gov.il>
2025-06-03 18:19:54 +0300

    style

--- psagot-client/src/components/CourseTopicGrid.jsx
+++ psagot-client/src/components/CourseTopicGrid.jsx
@@ -73,27 +73,27 @@ export default function CourseTopicGrid() {
     );
 
     const [dialogOpen, setDialogOpen] = React.useState(false);
-const [selectedTopic, setSelectedTopic] = React.useState(null);
+    const [selectedTopic, setSelectedTopic] = React.useState(null);
 
-const handleEditClick = (topic) => {
-  setSelectedTopic(topic); // שומרים את נושא הקורס שנבחר לעריכה
-  setDialogOpen(true);     // פותחים את הדיאלוג
-};
+    const handleEditClick = (topic) => {
+        setSelectedTopic(topic); // שומרים את נושא הקורס שנבחר לעריכה
+        setDialogOpen(true);     // פותחים את הדיאלוג
+    };
 
-const handleDialogClose = () => {
-  setDialogOpen(false);
-  setSelectedTopic(null);
-};
+    const handleDialogClose = () => {
+        setDialogOpen(false);
+        setSelectedTopic(null);
+    };
 
-const handleDialogSubmit = (formData) => {
-  console.log("נתוני עריכה:", formData);
-  setDialogOpen(false);
-  setSelectedTopic(null);
-};
+    const handleDialogSubmit = (formData) => {
+        console.log("נתוני עריכה:", formData);
+        setDialogOpen(false);
+        setSelectedTopic(null);
+    };
 
     return (
         <Box sx={{ width: '100%', marginTop: '8px', }}>
-            <TableContainer component={Paper} sx={{width: 'unset', borderRadius: '10px', p: "30px 20px 10px 20px" }}>
+            <TableContainer component={Paper} sx={{ width: 'unset', borderRadius: '10px', p: "30px 20px 10px 20px" }}>
                 <Table>
                     <TableHead>
                         <TableRow>
@@ -147,10 +147,10 @@ const handleDialogSubmit = (formData) => {
                                     </Box>
                                 </StyledTableCell>
                                 <StyledTableCell align="center">
-  <IconButton sx={{ bgcolor: '#F4F4F4' }} onClick={() => handleEditClick(topic)}>
-    <img src={editSvg} alt='edit_icon' style={{ marginTop: '0px' }} />
-  </IconButton>
-</StyledTableCell>
+                                    <IconButton sx={{ bgcolor: '#F4F4F4' }} onClick={() => handleEditClick(topic)}>
+                                        <img src={editSvg} alt='edit_icon' style={{ marginTop: '0px' }} />
+                                    </IconButton>
+                                </StyledTableCell>
                                 <StyledTableCell>
                                     <IconButton sx={{ bgcolor: '#F4F4F4' }}>
                                         <img src={deleteSvg} alt='delete_icon' style={{ marginTop: '0px' }} />
@@ -205,11 +205,11 @@ const handleDialogSubmit = (formData) => {
                 </Grid2>
             </Box>
             <TopicDialog
-  open={dialogOpen}
-  onClose={handleDialogClose}
-  onSubmit={handleDialogSubmit}
-  initialData={selectedTopic}
-/>
+                open={dialogOpen}
+                onClose={handleDialogClose}
+                onSubmit={handleDialogSubmit}
+                initialData={selectedTopic}
+            />
         </Box>
     );
 }
