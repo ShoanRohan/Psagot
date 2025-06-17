57a4c12e3cb48f80e4586360b1d055a92ab04828
BatelCohen18 <cohenbatel18@gmail.com>
2025-06-18 01:53:42 +0300

    Updated backend files

---
+++ Entities/DTO/TopicDTO.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class TopicDTO
+    {
+        public int TopicId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int TeacherId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int? NumberOfMeetings { get; set; }
+
+        public bool Computers { get; set; }
+
+        public bool Projector { get; set; }
+
+        public bool Microphone { get; set; }
+        public int? StatusId { get; set; }
+
+     
+    }
+}
