97f3718e4393860983d8a28b65b0e3e333ccbf79
BatelCohen18 <cohenbatel18@gmail.com>
2025-06-18 01:36:40 +0300

    merge conflicts

--- psagot-client/src/routers/AppRouter.js
+++ psagot-client/src/routers/AppRouter.js
@@ -1,37 +1,55 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Layout from '../pages/Layout';
 import CoursesPage from '../pages/CoursesPage';
-import Login from '../components/Login';
-import Register from '../components/Register';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
-import MeetingTable from '../components/MeetingTable';
+import MeetingPage from '../pages/MeetingPage';
+import UserManagement from '../pages/UserManagement';
+import MeetingForm from '../components/MeetingForm';
 
 const AppRouter = () => {
-    return (
+    const navigate = useNavigate(); 
+
+    // Handle navigation to edit meeting
+   const handleEditMeeting = (meeting) => {
+    // Navigate with meeting ID and optionally pass meeting data via state
+    navigate(`/edit-meeting/${meeting.meetingId}`, { 
+      state: { meeting } 
+    });
+  };
+
+    return (         
         <Routes>
-            <Route path='/' element={<Layout />}>
+            {/* Protected routes wrapped by Layout */}
+            <Route path="/" element={<Layout />}>
+                {/* Default route when accessing '/' */}
                 <Route index element={<HomePage />} />
-                <Route path='courses' element={<CoursesPage />} />
-                 <Route path='meetings' element={<MeetingTable />} /> 
+                
+                {/* Main application routes */}
+                <Route path="courses" element={<CoursesPage />} />
+                 <Route 
+        path="/meetings" 
+        element={<MeetingPage onEdit={handleEditMeeting} />} 
+      />
+      <Route 
+        path="/edit-meeting/:meetingId" 
+        element={<MeetingForm />} 
+      />
+      <Route 
+        path="/add-meeting" 
+        element={<MeetingForm />} 
+      />
+                
+                {/* Future routes - uncomment when ready */}
+                {/* <Route path="rooms" element={<RoomsPage />} /> */}
+                {/* <Route path="calendar" element={<CalendarPage />} /> */}
             </Route>
 
-            <Route path='' element={<LoginPage />}>
-                <Route path='login' element={<Login />} />
-
-            </Route>
-            <Route path='' element={<RegisterPage />}>
-                <Route path='Register' element={<Register />} />
-
-            </Route>
-         
-           
-{/* 
-            <Route path='add-meeting' element={<AddMeeting />} />
-            <Route path="edit-meeting/:meetingId" element={<EditMeeting />} /> */}
-
+            {/* Standalone routes (authentication pages) */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
         </Routes>
     );
 };
