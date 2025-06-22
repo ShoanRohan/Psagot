c77c0e7942bc7d084889932ba1204bf49665e3fe
Ahuva-Swiatycki <A0556772926@gmail.com>
2025-06-23 00:07:07 +0300

    trying...

--- psagot-client/src/components/CoursesGrid.jsx
+++ psagot-client/src/components/CoursesGrid.jsx
@@ -127,8 +127,8 @@ const CourseGrid = ({ courses }) => {
           <GridActionsCellItem icon={<SaveIcon />} label="Save" />,
           <GridActionsCellItem icon={<CancelIcon />} label="Cancel" />,
         ] : [
-            <GridActionsCellItem icon={<img src={editIcon} alt='עריכה' />} label="Edit" onClick={handleEditClick(id)} />,
-          ];
+          <GridActionsCellItem icon={<img src={editIcon} alt='עריכה' />} label="Edit" onClick={handleEditClick(id)} />,
+        ];
       },
     },
   ];
@@ -238,18 +238,22 @@ const CourseGrid = ({ courses }) => {
           shape="rounded"
           siblingCount={0}
           size="small"
+          
           sx={{
             '& .MuiPaginationItem-root': {
               backgroundColor: 'transparent',
+               fontSize: '0.75vw',
             },
             '& .Mui-selected': {
               backgroundColor: 'var(--Neutral-10, #F6F7F9) !important',
               border: '0.5px solid var(--Neutral-20, #F0F1F3)',
               borderRadius: '3px',
+               fontSize: '0.75vw',
             },
             '& .MuiPaginationItem-previousNext': {
               border: '0.5px solid var(--Neutral-20, #F0F1F3)',
               borderRadius: '3px',
+               fontSize: '0.75vw',
             },
           }}
         />
@@ -270,7 +274,7 @@ const CourseGrid = ({ courses }) => {
           flexDirection: 'column',
           alignItems: 'center',
           alignSelf: 'stretch',
-          padding: "1.2% 1% 2% 2%",
+          padding: "0.2% 1% 2% 2%",
           borderRadius: '10px',
           backgroundColor: "#FFF",
           boxShadow: '0px 0px 4px rgba(220, 226, 236, 0.8)',
@@ -280,13 +284,31 @@ const CourseGrid = ({ courses }) => {
           boxSizing: 'border-box',
           overflow: 'hidden',
           border: "none",
+
           "& .MuiDataGrid-root": {
             border: "none",
           },
         }}
 
       >
-        <Box sx={{ flexGrow: 1, width: '100%', position: 'relative', height: '100%' }}>
+        <Box sx={{
+          flexGrow: 1,
+          width: '100%',
+          position: 'relative',
+          height: '100%',
+          overflow: 'auto',
+          
+          '&::-webkit-scrollbar': {
+            width: '8px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            Color: 'var(--Brand-60, #326DEF)',
+            borderRadius: '8px',
+          },
+          '&::-webkit-scrollbar-track': {
+            backgroundColor: '#f0f0f0',
+          },
+        }}>
 
           <Box sx={{
             position: 'absolute',
@@ -296,17 +318,7 @@ const CourseGrid = ({ courses }) => {
             width: '0px',
             zIndex: 3
           }}>
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="2"
-              height="100%"
-              viewBox="0 0 2 100"
-              preserveAspectRatio="none"
-              fill="none"
-              style={{ height: '12%' }}
-            >
-              <path d="M1 0L1 100" stroke="#326DEF" strokeWidth="2" strokeLinecap="round" />
-            </svg> */}
+
           </Box>
           <DataGrid
             sx={{
