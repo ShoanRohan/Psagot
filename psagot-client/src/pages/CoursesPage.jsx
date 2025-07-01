4db10f2cec2886f26ab461f8e0dfb42a46b63ceb
YafitShama <yafit.h2112@gmail.com>
2025-07-01 19:00:18 +0300

    TopicDialog

--- psagot-client/src/pages/CoursesPage.jsx
+++ psagot-client/src/pages/CoursesPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Button, Typography, Container } from "@mui/material";
+import {
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
 import CourseGrid from "../components/CoursesGrid";
