bc22a93af962e543572d27d3b19d93a05cc8d026
EsterSimon <esty7858@gmail.com>
2025-07-02 21:54:52 +0300

    הוחזר הנב לינק

--- psagot-client/src/components/SideBar.jsx
+++ psagot-client/src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
