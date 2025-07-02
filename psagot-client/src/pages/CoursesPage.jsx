0ffaf285791679fac7f1fd75da893a3b490e5abd
YafitShama <yafit.h2112@gmail.com>
2025-07-02 08:26:11 +0300

    reCouresPage

--- psagot-client/src/pages/CoursesPage.jsx
+++ psagot-client/src/pages/CoursesPage.jsx
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  Box,
-  Button,
-  Typography,
-  Container
-} from "@mui/material";
+import { Box, Button, Typography, Container } from "@mui/material";
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
-
 import CourseGrid from "../components/CoursesGrid";
-import CourseSearch from "../components/CourseSearch";
-import TopicDialog from "../components/TopicDialog"; // ייבוא הקומפוננטה של הפופ־אפ
-
 import { filterCourses } from "../features/course/courseActions";
-
+import CourseSearch from "../components/CourseSearch";
 import circlePlus from "../assets/icons/circle-plus.png";
 import exptExcel from "../assets/icons/excelExport.png";
+import { styled } from "@mui/system";
+
 
 const CoursesPage = () => {
   const dispatch = useDispatch();
   const courses = useSelector((state) => state.course.courses);
-
-  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     dispatch(filterCourses({ statusId: 1 }));
@@ -45,17 +36,10 @@ const CoursesPage = () => {
     saveAs(data, "Courses.xlsx");
   };
 
-  const handleOpenDialog = () => {
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
 
   return (
     <Container
-      maxWidth={false}
+      maxWidth={false} 
       sx={{
         maxWidth: "none !important",
         width: "100%",
@@ -64,117 +48,106 @@ const CoursesPage = () => {
         flexDirection: "column",
         background: "var(--Brand-10, #FAFCFF)",
         position: "relative",
+        
+
       }}
     >
-      {/* כותרת + כפתורים */}
-      <Box
+<Box
+  sx={{
+    marginTop: "3%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: "2% 1.25%", 
+    boxSizing: "border-box",
+  }}
+>
+ 
+  <Typography
+    variant="h1"
+    sx={{
+      color: "var(--Brand-90, #0D1783)",
+      textAlign: "right",
+      fontFamily: "Rubik",
+      fontSize: "4.5vh",
+      fontStyle: "normal",
+      fontWeight: 700,
+      lineHeight: "normal",
+      textTransform: "capitalize",
+    }}
+  >
+    קורסים
+  </Typography>
+
+
+  <Box
+    sx={{
+      display: "flex",
+      flexDirection: "row-reverse",
+      alignItems: "center",
+      gap: "16px", 
+    }}
+  >
+
+    <Button
+      variant="contained"
+      sx={{
+        width: "152px",
+        height: "40px",
+        borderRadius: "50vh",
+        padding: "0 16px",
+        gap: "8px",
+        backgroundColor: "#326DEF",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minWidth: "152px",
+        minHeight: "40px",
+      }}
+    >
+      <img src={circlePlus} alt="הוספת קורס" style={{ width: "16px", height: "16px" }} />
+      <Typography
         sx={{
-          marginTop: "3%",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "100%",
-          padding: "2% 1.25%",
-          boxSizing: "border-box",
+          fontFamily: "Rubik",
+          fontWeight: 400,
+          fontSize: "16px",
+          lineHeight: "100%",
+          textAlign: "center",
+          color: "#FFFFFF",
+          textTransform: "capitalize",
         }}
       >
-        <Typography
-          variant="h1"
-          sx={{
-            color: "var(--Brand-90, #0D1783)",
-            textAlign: "right",
-            fontFamily: "Rubik",
-            fontSize: "4.5vh",
-            fontStyle: "normal",
-            fontWeight: 700,
-            lineHeight: "normal",
-            textTransform: "capitalize",
-          }}
-        >
-          קורסים
-        </Typography>
+        הוספת קורס
+      </Typography>
+    </Button>
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row-reverse",
-            alignItems: "center",
-            gap: "16px",
-          }}
-        >
-          {/* כפתור הוספת קורס */}
-          <Button
-            variant="contained"
-            onClick={handleOpenDialog}
-            sx={{
-              width: "152px",
-              height: "40px",
-              borderRadius: "50vh",
-              padding: "0 16px",
-              gap: "8px",
-              backgroundColor: "#326DEF",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              minWidth: "152px",
-              minHeight: "40px",
-            }}
-          >
-            <img
-              src={circlePlus}
-              alt="הוספת קורס"
-              style={{ width: "16px", height: "16px" }}
-            />
-            <Typography
-              sx={{
-                fontFamily: "Rubik",
-                fontWeight: 400,
-                fontSize: "16px",
-                lineHeight: "100%",
-                textAlign: "center",
-                color: "#FFFFFF",
-                textTransform: "capitalize",
-              }}
-            >
-              הוספת קורס
-            </Typography>
-          </Button>
+    <Button
+      onClick={exportToExcel}
+      sx={{
+        width: "40px",
+        height: "40px",
+        borderRadius: "8px",
+        padding: "0",
+        backgroundColor: "#F0F1F3",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minWidth: "40px",
+        minHeight: "40px",
+      }}
+    >
+      <img src={exptExcel} alt="ייצוא לאקסל" style={{ width: "24px", height: "24px" }} />
+    </Button>
+  </Box>
+</Box>
 
-          {/* כפתור ייצוא לאקסל */}
-          <Button
-            onClick={exportToExcel}
-            sx={{
-              width: "40px",
-              height: "40px",
-              borderRadius: "8px",
-              padding: "0",
-              backgroundColor: "#F0F1F3",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              minWidth: "40px",
-              minHeight: "40px",
-            }}
-          >
-            <img
-              src={exptExcel}
-              alt="ייצוא לאקסל"
-              style={{ width: "24px", height: "24px" }}
-            />
-          </Button>
-        </Box>
-      </Box>
 
-      {/* חיפוש קורסים */}
-      <CourseSearch />
+      <CourseSearch  />
 
-      {/* טבלת קורסים */}
       <CourseGrid courses={courses} />
-
-      {/* פופאפ של הוספת / עריכת קורס */}
-      <TopicDialog open={openDialog} onClose={handleCloseDialog} initialData={{}} />
-    </Container>
+     </Container>
   );
 };
 
