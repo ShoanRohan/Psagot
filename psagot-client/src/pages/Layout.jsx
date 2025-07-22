0ab5fa3bac9c2a47a7320cadeef21832b36ccce1
chayunyan11 <chaya0583217081@gmail.com>
2025-07-22 18:35:38 +0300

    change

--- psagot-client/src/pages/Layout.jsx
+++ psagot-client/src/pages/Layout.jsx
@@ -7,9 +7,9 @@ import { Box } from "@mui/material";
 const Layout = () => {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <Header />
+           
             <Section />
-            <Footer />
+          
         </Box>
     );
 }
